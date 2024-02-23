7ee07e8c4d45ac4b263c6d4335a74853f2b2d747
Tangi Le Henaff <tangi_lehenaff@ens.univ-artois.fr>
2024-02-23 13:19:23 +0100

    debut du challenge 1

--- CPL.hs
+++ CPL.hs
@@ -8,8 +8,3 @@ data Formula = T
     |   Eqv Formula Formula
     deriving Show
 
-constraint :: Formula
-constraint = And (Or (Var "p1") (Var "p2")) (Or (Var "t1") (Var "t2"))
-
-reglement :: Formula a => a -> a -> a
-reglement f1 f2 = Or (And (f1) (not f2)) (And (not f1) (f2))  
--- Cha1.hs
+++ Cha1.hs
@@ -1,2 +1,15 @@
 import CPL
 
+constraint :: Formula 
+constraint = And (Or (Var "p1") (Var "p2")) (Or (Var "t1") (Var "t2"))
+
+reglement :: Formula a => a -> a -> a
+reglement f1 f2 = Or (And (f1) (Not f2)) (And (Not f1) (f2))  
+
+Formule1 :: Formula
+Formule1 = And (Var "p1") (Var "t2")
+
+Formule2 :: Formula
+Formule2 = And (Or (Var "p1") (Var "p2")) (Or (Var "t1") (Var "t2"))
+
+challenge1 :: And (reglement ())
