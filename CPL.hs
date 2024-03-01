7399ca0ddb2caeb178732d48a44166878356d05f
Tangi Le Henaff <tangi_lehenaff@ens.univ-artois.fr>
2024-03-01 13:01:53 +0100

    ajout genAllWorlds

--- CPL.hs
+++ CPL.hs
@@ -9,9 +9,36 @@ data Formula = T
     |   Eqv Formula Formula
     deriving Show
 
-data World = [] | String : World
-    deriving Show
+type World = [String]
 
-genAllWorlds:: World -> [World]
-genAllWorlds [] = []
-genAllWorlds x:xs = x : genAllWorlds xs
+genAllWorlds :: World -> [World]
+genAllWorlds a = (genAllWorldsRec a)++genAllList a
+
+genAllWorldsRec:: World -> [World]
+genAllWorldsRec [] = []
+genAllWorldsRec (x:xs) = forEachAddList x (forEachAddList x ((genAllList xs))++genAllWorldsRec xs) ++ genAllWorldsRec xs
+
+-- ajoute a a chaque liste dans la liste, empeche les doublons
+forEachAddList ::Eq a => a -> [[a]] -> [[a]]
+forEachAddList _ []=[]
+forEachAddList a [[]]=[[a]]
+forEachAddList a (x:xs)
+    |not (isInList a x)=(a:x):(forEachAddList a xs)
+    |otherwise = x:(forEachAddList a xs)
+
+testForEachAddList=[forEachAddList 1 []==[],
+    forEachAddList 1 [[1],[2]]==[[1,1],[1,2]]]
+
+isInList :: Eq a => a -> [a] -> Bool
+isInList _ []=False
+isInList a (x:xs)
+    |a==x=True
+    |otherwise=isInList a xs
+
+testIsInList=[isInList 5 []==False,
+    isInList 4 [1,2]==False,
+    isInList 4 [1,2,3,4]==True]
+
+genAllList:: [a] -> [[a]]
+genAllList []=[]
+genAllList (x:xs)=[x]:(genAllList xs)
