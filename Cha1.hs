bfdffe5382a03bc09ddf8152dd540028701d76f9
Tangi Le Henaff <tangi_lehenaff@ens.univ-artois.fr>
2024-03-12 17:17:26 +0100

    contrainte  corrigee

--- Cha1.hs
+++ Cha1.hs
@@ -1,7 +1,7 @@
 import CPL
 
 constraint :: Formula 
-constraint = And (And (Imp (Var "p1") (Not (Var "t1"))) (Imp (Var "t1") (Not (Var "p1")))) (And (Imp (Var "p2") (Not (Var "t2"))) (Imp (Var "t2") (Not (Var "p2"))))
+constraint =And (And (And (Imp (Var "p1") (Not (Var "t1"))) (Imp (Var "t1") (Not (Var "p1")))) (And (Imp (Var "p2") (Not (Var "t2"))) (Imp (Var "t2") (Not (Var "p2"))))) (And (Or (Var "p1") (Var "t1")) (Or (Var "p2")(Var "t2")) )
 
 reglement :: Formula -> Formula -> Formula 
 reglement f1 f2 = Or (And (f1) (Not f2)) (And (Not f1) (f2))  
--- Cha5.hs
+++ Cha5.hs
@@ -1,3 +1,4 @@
+module Challenge5 where
 import CPL
 
 constraint :: Formula 
@@ -5,13 +6,13 @@ constraint =And (And (And (Imp (Var "p1") (Not (Var "t1"))) (Imp (Var "t1") (Not
 
 
 reglement :: Formula -> Formula -> Formula 
-reglement f1 f2 = Or (And (f1) (f2)) (And (Not f1) ( Not f2))
+reglement f1 f2 = And (And (Imp (Var "p1") (f1)) (Imp (Var "t1")(Not f1))) (And (Imp (Var "p2") (Not f2)) (Imp (Var "t2") (f2)))
 
 porte1 :: Formula
-porte1 = Or (And (Var "t1") (Not (Var "p2"))) (And (Var "p2") (Not (Var "t1")))
+porte1 = Or (Imp (Var "p1")(Var "t2")) (Imp (Var "t1")(Var "p2"))
 
 porte2 :: Formula
-porte2 = Var "p1"
+porte2 = And (Var "t2") (Var "p1")
 
-challenge3 :: Formula
-challenge3 = And (reglement porte1 porte2) constraint
+challenge5 :: Formula
+challenge5 = And (reglement porte1 porte2) constraint
